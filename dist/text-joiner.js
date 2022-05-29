45c0ee2ec92c45531de67aea975daccf38d3873b
2cheol <godicheol@gmail.com>
2022-05-30 04:33:00 +0900

    3

--- dist/text-joiner.js
+++ dist/text-joiner.js
@@ -15,7 +15,93 @@
         return lineBreakers[config.lineBreaker];
     }
 
-    function setLineBreaker(str) {
+    function getString(arr, cb) {
+        var index = arr.length;
+        var count = 0;
+        var queue = [];
+        var recursiveFn = function() {
+            if (index > count) {
+                // execute
+                var elem = arr[count];
+                if (typeof(elem) == "string") {
+                    // String
+                    queue.push(elem);
+                    count++;
+                    recursiveFn();
+                } else if (elem instanceof Node && elem.tagName.toUpperCase() == "TEXTAREA") {
+                    // <textarea>
+                    queue.push(elem.value);
+                    count++;
+                    recursiveFn();
+                } else if (elem instanceof Node && elem.tagName.toUpperCase() == "INPUT" && elem.getAttribute("type") == "text") {
+                    // <input type="text">
+                    queue.push(elem.value);
+                    count++;
+                    recursiveFn();
+                } else if (elem instanceof Node && elem.tagName.toUpperCase() == "INPUT" && elem.getAttribute("type") == "file") {
+                    var files = [];
+                    for (var i = 0; i < elem.files.length; i++) {
+                        // sort multiple files
+                        if (
+                            elem.files[i].type != "text/plain" ||
+                            !elem.files[i] instanceof Blob ||
+                            typeof(elem.files[i].name) == "undefined"
+                        ) {
+                            continue;
+                        }
+                        files.push(elem.files[i]);
+                    }
+                    
+                    files.sort(function(a, b) {
+                        if (!String.localeCompare) {
+                            return a.name - b.name;
+                        } else {
+                            return a.name.localeCompare(b.name, undefined, {
+                                sensitivity: "base",
+                                numeric: true,
+                            });
+                        }
+                    });
+
+                    var index2 = files.length;
+                    var count2 = 0;
+                    var recursiveFn2 = function() {
+                        if (index2 > count2) {
+                            var file = elem.files[count2];
+                            var reader = new FileReader();
+                            reader.onload = function(e) {
+                                queue.push(e.target.result);
+                                count2++;
+                                recursiveFn2();
+                            };
+                            reader.onerror = function(e) {
+                                console.error("Load error");
+                                count2++;
+                                recursiveFn2();
+                            };
+                            reader.readAsText(file, "UTF-8");
+                        } else {
+                            count++;
+                            recursiveFn();
+                        }
+                    }
+                    recursiveFn2();
+                } else {
+                    console.error("Element not valid");
+                    count++;
+                    recursiveFn();
+                }
+            } else {
+                // end
+                return cb(null, queue);
+            }
+        }
+        recursiveFn();
+    }
+
+    var exports = {};
+
+    exports.setLineBreaker = function(str) {
         switch(str.toUpperCase()) {
             case "CR":
                 config.lineBreaker = "CR";
@@ -29,39 +115,36 @@
         }
     }
 
-    function joinString(arr) {
+    exports.join = function(arr, cb) {
         if (!Array.isArray(arr)) {
-            console.error("Parameter is not string.");
+            console.error("Parameter is not Array.");
             return false;
         }
-
-        var str = "";
-        for (var i = 0; i < arr.length; i++) {
-            var elem = arr[i];
-
-            if (typeof(elem) != "string") {
-                continue;
+    
+        getString(arr, function(err, res) {
+            if (err) {
+                return cb(err);
             }
 
-            // is not start
-            if (i != 0) {
-                str += getLineBreaker();
+            var str = "";
+            for (var i = 0; i < res.length; i++) {
+                var elem = res[i];
+    
+                // is not start
+                if (i != 0) {
+                    str += getLineBreaker();
+                }
+                
+                // main
+                str += elem;
             }
-
-            // main
-            str += elem;
-        }
-
-        return str;
+    
+            return cb(null, str);
+        });
     }
 
-    var exports = {
-        setLineBreaker: setLineBreaker,
-        join: joinString,
-    }
-
-    if (typeof(window.textJoiner) === "undefined") {
-        window.textJoiner = exports;
+    if (typeof(window.tj) === "undefined") {
+        window.tj = exports;
     }
 
     // polyfill
